b88eea4d941b6d580286feee826bf305ecf01354
skistua <b166er.dada@gmail.com>
2015-02-01 00:27:02 +0800

    finished Object pool service

--- bruce/Assets/Script/base/objectPool/ObjectPoolService.cs
+++ bruce/Assets/Script/base/objectPool/ObjectPoolService.cs
@@ -8,8 +8,85 @@ class ObjectPoolService
 
     private ObjectPoolService()
     {
-        _poolArray = new Dictionary
+		_poolArray = new Dictionary<Type,ObjectPool> ();
     }
 
+	public static ObjectPoolService Instance()
+	{
+		return _instance;
+	}
+
+	public Object borrowObject(Type type)
+	{
+		ObjectPool pool = null;
+
+		if (!_poolArray.ContainsKey (type)) 
+		{
+			_poolArray [type] = pool = new ObjectPool (type);
+		} else 
+		{
+			pool = _poolArray [type];
+		}
+
+		return pool.borrowObject();
+	}
+
+	public void returnObject(Object obj)
+	{
+		Type type = typeof(obj);
+
+		if(_poolArray.ContainsKey(type))
+		{
+			_poolArray[type].returnObject(obj);
+		}
+		else
+		{
+			ObjectPool pool = new ObjectPool(type);
+			pool.returnObject(obj);
+			_poolArray[type] = pool;
+		}
+	}
+
+	public void clearPool(Type type)
+	{
+		if (_poolArray.ContainsKey (type)) 
+		{
+			_poolArray[type].cleanObject();	
+		}
+	}
+
+	public int getPoolNum(Type type)
+	{
+		if(_poolArray.ContainsKey(type))
+		{
+			return _poolArray[type].getObjectNum;
+		}
+		return 0;
+	}
+
+	public ObjectPool getObjectPool(Type type)
+	{
+		if(_poolArray.ContainsKey(type))
+		{
+			return _poolArray[type];
+		}
+		else
+		{
+			ObjectPool pool = new ObjectPool(type);
+			_poolArray[type] = pool;
+			return pool;
+		}
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
 
