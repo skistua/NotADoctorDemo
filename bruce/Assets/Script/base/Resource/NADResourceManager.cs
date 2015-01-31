81ffe56df9d5c9668795e25bcc7cb4b23ea91aaa
Mustapha <b166er.dada@gmail.com>
2015-01-31 19:48:07 +0800

    add resource base script

---
+++ bruce/Assets/Script/base/Resource/NADResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 资源管理器 从本地或者远端服务器加载 Assetbundle资源
+/// </summary>
+public class NADResourceManager : MonoBehaviour {
+
+	public GameObject requestResrouceImmediate(string type, int id)
+    {
+        return null;
+    }
+
+    public GameObject[] requestResourceGroupImmediate(string type, int id)
+    {
+        return null;
+    }
+
+    public GameObject requesResourceFromGroupImmediate(string type, int id, int subid)
+    {
+        return null;
+    }
+}
